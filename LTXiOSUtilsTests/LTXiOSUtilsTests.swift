3d57eee9cef28c131c047c915122a7a5579cdfb4
李天星 <litianxing@topscomm.com>
2019-08-02 15:41:35 +0800

    new project

---
+++ LTXiOSUtilsTests/LTXiOSUtilsTests.swift
@@ -0,0 +1,34 @@
+//
+//  LTXiOSUtilsTests.swift
+//  LTXiOSUtilsTests
+//
+//  Created by 李天星 on 2019/8/2.
+//  Copyright © 2019年 李天星. All rights reserved.
+//
+
+import XCTest
+@testable import LTXiOSUtils
+
+class LTXiOSUtilsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
