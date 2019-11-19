b6869a4aceea8164a2c31e3abc5c1134c71b96e3
李天星 <litianxing@topscomm.com>
2019-11-19 19:13:09 +0800

    启动页、图标以及导航

--- LTXiOSUtilsDemo/LTXiOSUtilsDemo/System/AppDelegate.swift
+++ LTXiOSUtilsDemo/LTXiOSUtilsDemo/System/AppDelegate.swift
@@ -14,7 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if window == nil {
+            window = UIWindow()
+        }
+        let rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
