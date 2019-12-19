bef2a9676e1fbb79816d716efd83d5005662168a
CoderStar <1340529758@qq.com>
2019-12-19 15:06:35 +0800

    时间戳

--- LTXiOSUtilsDemo/LTXiOSUtilsDemoTests/LTXiOSUtilsDemoTests.swift
+++ LTXiOSUtilsDemo/LTXiOSUtilsDemoTests/LTXiOSUtilsDemoTests.swift
@@ -8,28 +8,28 @@
 
 import XCTest
 import LTXiOSUtils
+import QorumLogs
 @testable import LTXiOSUtilsDemo
 
 class LTXiOSUtilsDemoTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
     }
 
     func testExample() {
-        print(Date().formatDate(format: .MDHM))
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        QL1(Date().weekDay)
+
     }
 
     func testPerformanceExample() {
-        // This is an example of a performance test case.
+
         self.measure {
-            // Put the code you want to measure the time of here.
+
         }
     }
 
