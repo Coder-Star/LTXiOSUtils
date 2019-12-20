c33d5e0a41aacf3b448749e5cb38abe91c4add63
CoderStar <1340529758@qq.com>
2019-12-20 08:52:06 +0800

    .

---
+++ LTXiOSUtils/Classes/Constants/KeyEnum.swift
@@ -0,0 +1,14 @@
+//
+//  KeyEnum.swift
+//  LTXiOSUtils
+//  iOS内部关键key值
+//  Created by 李天星 on 2019/12/20.
+//
+
+import Foundation
+
+/// iOS内部关键key值
+enum KeyEnum {
+    /// webview进度条keypath
+    public static let webViewProgressKeyPath = "estimatedProgress"
+}
