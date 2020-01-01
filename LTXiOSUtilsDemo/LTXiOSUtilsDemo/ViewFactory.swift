b445116aeb9c588929c2ef21c3523181df837a8c
CoderStar <1340529758@qq.com>
2020-01-01 20:33:12 +0800

    。

---
+++ LTXiOSUtilsDemo/LTXiOSUtilsDemo/ViewFactory.swift
@@ -0,0 +1,21 @@
+//
+//  ViewFactory.swift
+//  LTXiOSUtilsDemo
+//
+//  Created by 李天星 on 2020/1/1.
+//  Copyright © 2020 李天星. All rights reserved.
+//
+
+import Foundation
+
+/// 快速创建控件
+public class ViewFactory {
+
+}
+
+public extension ViewFactory {
+    /// 创建UIlabel
+    func createLabel() {
+
+    }
+}
