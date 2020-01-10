6c033b619afe6981d8e4b292ea4a9d472466c750
CoderStar <1340529758@qq.com>
2020-01-10 21:33:45 +0800

    .

---
+++ LTXiOSUtilsDemo/LTXiOSUtilsDemoTests/ThirdLibTest/KeychainAccessTest.swift
@@ -0,0 +1,39 @@
+//
+//  KeychainAccessTest.swift
+//  LTXiOSUtilsDemoTests
+//
+//  Created by 李天星 on 2020/1/10.
+//  Copyright © 2020 李天星. All rights reserved.
+//
+
+import XCTest
+import KeychainAccess
+import QorumLogs
+
+@testable import KeychainAccess
+
+class KeychainAccessTest: XCTestCase {
+
+    let keychain = Keychain(service: "com.coderstar.ltxiosutilsdemo").accessibility(.afterFirstUnlock)
+
+    func test() {
+//        save()
+        get()
+        remove()
+    }
+
+    func save() {
+        keychain[string:"info"] = "处理"
+    }
+
+    func get() {
+        QL1(keychain[string:"info"])
+        QL1(keychain[string:"info1"])
+    }
+
+    func remove() {
+        try? keychain.remove("info")
+    }
+}
+
+
