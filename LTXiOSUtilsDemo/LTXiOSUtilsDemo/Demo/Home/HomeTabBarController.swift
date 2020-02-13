5491e20c7326f8c6af0181333de0f31ccf194d36
李天星 <litianxing@topscomm.com>
2020-02-13 18:41:30 +0800

    .

--- LTXiOSUtilsDemo/LTXiOSUtilsDemo/Demo/Home/HomeTabBarController.swift
+++ LTXiOSUtilsDemo/LTXiOSUtilsDemo/Demo/Home/HomeTabBarController.swift
@@ -53,13 +53,3 @@ extension HomeTabBarController: UITabBarControllerDelegate {
         QL1(viewController)
     }
 }
-
-class HomeNavigationController: UINavigationController {
-
-    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
-        if viewControllers.count > 0 {
-            viewController.hidesBottomBarWhenPushed = true
-        }
-        super.pushViewController(viewController, animated: animated)
-    }
-}
