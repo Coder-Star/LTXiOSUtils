07f3ed455a03b72fe5c1c592c19365eb1fe953ac
李天星 <litianxing@topscomm.com>
2020-02-17 15:10:42 +0800

    .

--- LTXiOSUtilsDemo/LTXiOSUtilsDemo/Demo/Home/DemoListViewController.swift
+++ LTXiOSUtilsDemo/LTXiOSUtilsDemo/Demo/Home/DemoListViewController.swift
@@ -11,6 +11,38 @@ import LTXiOSUtils
 
 class DemoListViewController: BaseGroupTableMenuViewController {
 
+    let titles = ["创建群聊", "加好友/群", "扫一扫", "面对面快传", "付款", "拍摄"]
+    let images = [UIImage(named: "right_menu_multichat_white"),
+                   UIImage(named: "right_menu_addFri_white"),
+                   UIImage(named: "right_menu_QR_white"),
+                   UIImage(named: "right_menu_facetoface_white"),
+                   UIImage(named: "right_menu_payMoney_white"),
+                   UIImage(named: "right_menu_sendvideo_white")]
+    lazy var menuView: FWMenuView = {
+        let vProperty = FWMenuViewProperty()
+        vProperty.popupCustomAlignment = .topRight
+        vProperty.popupAnimationType = .scale
+        vProperty.maskViewColor = .clear
+        vProperty.popupViewEdgeInsets = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 8)
+        vProperty.topBottomMargin = 0
+        vProperty.animationDuration = 0.3
+        vProperty.popupArrowStyle = .round
+        vProperty.popupArrowVertexScaleX = 0.95
+        vProperty.backgroundColor = UIColor(hexString: "#403F42")
+        vProperty.splitColor = UIColor(hexString: "#403F42")
+        vProperty.separatorColor = UIColor(hexString: "#5B5B5D")
+        vProperty.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white,
+                                         NSAttributedString.Key.backgroundColor: UIColor.clear,
+                                         NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15.0)]
+        vProperty.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+
+        let menuView = FWMenuView.menu(itemTitles: titles, itemImageNames: images as? [UIImage], property: vProperty, itemBlock: { (_, index, _) in
+            QL1("Menu：点击了第\(index)个按钮")
+        })
+//        menuView.attachedView = self.baseView
+        return menuView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let addBarButton = UIBarButtonItem(image: R.image.add(), style: .plain, target: self, action: #selector(self.add))
@@ -18,7 +50,7 @@ class DemoListViewController: BaseGroupTableMenuViewController {
     }
 
     @objc private func add() {
-        QL1("添加")
+        menuView.show()
     }
 
     override func setMenu() {
