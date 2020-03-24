e729962dc7fdef360df09c747d3ddfb10dec4fa9
CoderStar <1340529758@qq.com>
2020-03-24 15:02:28 +0800

    调整结构

--- LTXiOSUtils/Classes/Core/Extension/Core.swift
+++ LTXiOSUtils/Classes/Core/Extension/Core.swift
@@ -20,19 +20,34 @@ public struct Core<Base> {
 }
 
 /// 协议
-public protocol Compatible {
-    /// 关联
-    associatedtype CompatibleType
-    /// 生成核心
-    var core: CompatibleType { get }
+public protocol CoreCompatible {
+    /// 实例方法关联
+    associatedtype CoreCompatibleInstanceType
+    /// 类方法关联
+    associatedtype CoreCompatibleClassType
+
+    /// 实例方法，只读计算属性
+    var core: CoreCompatibleInstanceType { get }
+    /// 类方法，只读计算属性
+    static var core: CoreCompatibleClassType { get }
 }
-extension Compatible {
-    /// 生成核心实现
+/// Compatible协议默认实现
+extension CoreCompatible {
+    /// 实例方法默认实现
     public var core: Core<Self> {
         return Core(self)
     }
+
+    /// 类方法默认实现
+    public static var core: Core<Self>.Type {
+        return Core<Self>.self
+    }
 }
 
-extension NSObject: Compatible {}
+extension NSObject: CoreCompatible {}
 
-extension String: Compatible {}
+extension String: CoreCompatible {}
+extension Double: CoreCompatible {}
+extension Array: CoreCompatible {}
+extension Int: CoreCompatible {}
+extension Date: CoreCompatible {}
