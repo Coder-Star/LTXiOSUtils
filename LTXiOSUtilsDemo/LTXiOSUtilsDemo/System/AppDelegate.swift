ab45904111142ec28bbf1f9de3515e30ed824e36
李天星 <litianxing@topscomm.com>
2020-04-24 20:10:02 +0800

    资源工具类

--- LTXiOSUtilsDemo/LTXiOSUtilsDemo/System/AppDelegate.swift
+++ LTXiOSUtilsDemo/LTXiOSUtilsDemo/System/AppDelegate.swift
@@ -6,11 +6,6 @@
 //  Copyright © 2019年 李天星. All rights reserved.
 //
 
-/*
- 说一下PluggableAppDelegate这个库的原理：组合模式
-PluggableApplicationDelegate实现了UIApplicationDelegate协议，并在扩展中默认实现了所有方法，在生命周期的各个方法中调用了ApplicationService及其子类相对应生命周期的方法，从而实现解耦，其中ApplicationService也实现了UIApplicationDelegate协议。
- */
-
 import UIKit
 
 @UIApplicationMain
