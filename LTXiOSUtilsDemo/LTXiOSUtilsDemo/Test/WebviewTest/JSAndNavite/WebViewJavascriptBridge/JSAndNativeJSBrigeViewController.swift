8424f6a46e84a925298f3119b7a3ca69ec1f807e
李天星 <litianxing@topscomm.com>
2020-09-13 23:26:12 +0800

    WebViewJavascriptBridge Demo

--- LTXiOSUtilsDemo/LTXiOSUtilsDemo/Test/WebviewTest/JSAndNavite/WebViewJavascriptBridge/JSAndNativeJSBrigeViewController.swift
+++ LTXiOSUtilsDemo/LTXiOSUtilsDemo/Test/WebviewTest/JSAndNavite/WebViewJavascriptBridge/JSAndNativeJSBrigeViewController.swift
@@ -10,12 +10,27 @@ import Foundation
 import WebKit
 import LTXiOSUtils
 import SwiftyJSON
+import WebViewJavascriptBridge
+
+/**
+ WebViewJavascriptBridge基本原理
+ JS -> Native 通过decidePolicyFor拦截url获取信息
+ Native -> JS evaluateJavaScript
+
+ 因为WebViewJavascriptBridge需要借助decidePolicyFor通信，所以需要WebViewJavascriptBridge内部实现 WKNavigationDelegate 协议，不可将协议交给其他类进行实现，如果想实现 WKNavigationDelegate 协议，可以通过setWebViewDelegate方法将自身作为协议的实现
+ */
 
 class JSAndNativeJSBrigeViewController: WkWebViewController {
 
+    var webBridge: WKWebViewJavascriptBridge?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        webView.navigationDelegate = self
+        webBridge = WKWebViewJavascriptBridge.init(for: webView)
+
+        webBridge?.setWebViewDelegate(self)
+
+        iniNativeCall()
         guard let htmlURL = Bundle.main.url(forResource: "JSAndNativeJSBrige", withExtension: "html") else {
             return
         }
@@ -28,8 +43,23 @@ class JSAndNativeJSBrigeViewController: WkWebViewController {
         navigationItem.rightBarButtonItem = rightBarItem
     }
 
+    private func iniNativeCall() {
+        self.webBridge?.registerHandler("callForJS") { (param, callBack) in
+            Log.d(param)
+            Log.d(callBack)
+            guard let callBack = callBack else {
+                return
+            }
+            callBack("收到来自JS的字典参数，这是Native的回复")
+        }
+    }
+
     @objc
     func action() {
+        let param = ["message": "来自Native的参数"]
+        webBridge?.callHandler("callForNative", data: param) { result in
+            Log.d(result)
+        }
     }
 
 }
