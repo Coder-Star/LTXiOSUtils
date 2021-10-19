50a635478676339b5ae78864dad2b2b58c2bb4ee
CoderStar <1340529758@qq.com>
2021-10-19 23:52:23 +0800

    AsyncOperation修改，状态加锁

--- LTXiOSUtils/Classes/Util/AsyncOperation.swift
+++ LTXiOSUtils/Classes/Util/AsyncOperation.swift
@@ -7,59 +7,87 @@
 
 import Foundation
 
-class AsyncOperation: Operation {
-    private var asyncExecuting = false {
-        willSet {
-            willChangeValue(forKey: "isExecuting")
-        }
-        didSet {
-            didChangeValue(forKey: "isExecuting")
-        }
-    }
-
-    /// 内部控制Operation是否结束变量
-    ///
-    /// 需要手动进行KVO，否则completionBlock不会被触发，被依赖的Operation也不会开始
-    private var asyncFinished = false {
-        willSet {
-            willChangeValue(forKey: "isFinished")
-        }
-        didSet {
-            didChangeValue(forKey: "isFinished")
-        }
-    }
-
-
+public class AsyncOperation: Operation {
     private var block: ((_ operation: AsyncOperation) -> Void)?
+
+    private let queue = DispatchQueue(label: "async.operation.queue")
+    private let lock = NSLock()
+
+    private var _executing = false
+    private var _finished = false
 
     /// 数据
     ///
     /// 为Operation绑定一下数据，方便被依赖的Operation获取该Operation处理后的一些数据
     public var data: Any?
 
-    override func start() {
+    /// 是否执行
+    ///
+    /// 内部加锁保证线程安全
+    public override var isExecuting: Bool {
+        get {
+            lock.lock()
+            let wasExecuting = _executing
+            lock.unlock()
+            return wasExecuting
+        }
+        set {
+            if isExecuting != newValue {
+                willChangeValue(forKey: "isExecuting")
+                lock.lock()
+                _executing = newValue
+                lock.unlock()
+                didChangeValue(forKey: "isExecuting")
+            }
+        }
+    }
+
+    /// 是否结束
+    ///
+    /// 内部加锁保证线程安全
+    /// 需要手动进行KVO，否则completionBlock不会被触发，被依赖的Operation也不会开始
+    public override var isFinished: Bool {
+        get {
+            lock.lock()
+            let wasFinished = _finished
+            lock.unlock()
+            return wasFinished
+        }
+        set {
+            if isFinished != newValue {
+                willChangeValue(forKey: "isFinished")
+                lock.lock()
+                _finished = newValue
+                lock.unlock()
+                didChangeValue(forKey: "isFinished")
+            }
+        }
+    }
+
+    /// 标识该Operation是否以异步形式运行
+    public override var isAsynchronous: Bool {
+        return true
+    }
+
+    public override func start() {
         if isCancelled {
-            asyncFinished = true
+            isFinished = true
             return
         }
-        asyncExecuting = true
-        block?(self)
+
+        isExecuting = true
+
+        queue.async { [weak self] in
+            self?.main()
+        }
     }
 
-    override var isAsynchronous: Bool {
-        return true
-    }
-
-    override var isConcurrent: Bool {
-        return true
-    }
-
-    override var isFinished: Bool {
-        return asyncFinished
-    }
-
-    override var isExecuting: Bool {
-        return asyncExecuting
+    public override func main() {
+        if let block = block {
+            block(self)
+        } else {
+            finish()
+        }
     }
 }
 
@@ -72,7 +100,7 @@ extension AsyncOperation {
     }
 
     public func finish() {
-        asyncExecuting = false
-        asyncFinished = true
+        isExecuting = false
+        isFinished = true
     }
 }
