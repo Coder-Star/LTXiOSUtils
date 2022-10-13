26a9dae96f2f2ade17598995e4b7e8e9d556c35e
CoderStar <1340529758@qq.com>
2022-10-13 18:49:48 +0800

    文件

--- Sources/LTXiOSUtils/Classes/Util/Debouncer+Throttler.swift
+++ Sources/LTXiOSUtils/Classes/Util/Debouncer+Throttler.swift
@@ -9,8 +9,8 @@ import Foundation
 
 /// 防抖
 /// n 秒后在执行该事件，若在 n 秒内被重复触发，则重新计时
-/// 1、处理搜索框过于频繁发起网络请求的问题，每当用户输入一个字符的时候，都发起网络请求，会浪费一部分网络资源，通过debounce，可以实现，当用户停止输入0.5秒再发送请求。
-/// 2、处理按钮的连续点击问题，debounce只接收0.5秒后的最后一次点击事件，因此自动忽略了中间的多次连续点击事件
+/// 应用场景1、处理搜索框过于频繁发起网络请求的问题，每当用户输入一个字符的时候，都发起网络请求，会浪费一部分网络资源，通过debounce，可以实现，当用户停止输入0.5秒再发送请求。
+/// 应用场景2、处理按钮的连续点击问题，debounce只接收0.5秒后的最后一次点击事件，因此自动忽略了中间的多次连续点击事件
 public class Debouncer {
     private let queue: DispatchQueue
     private let interval: TimeInterval
@@ -39,6 +39,7 @@ public class Debouncer {
 
 /// 节流
 /// n 秒内只运行一次，若在 n 秒内重复触发，只有一次生效
+/// 应用场景1：监听页面滚动进行相关操作
 public class Throttler {
     private let queue: DispatchQueue
     private let interval: TimeInterval
