1bbe3997ceabf2a779cef8dea46d0d85031ed94b
CoderStar <1340529758@qq.com>
2025-07-22 23:29:25 +0800

    Update CSPaddingLabel.swift

--- Sources/LTXiOSUtils/Classes/Component/Class/CSPaddingLabel.swift
+++ Sources/LTXiOSUtils/Classes/Component/Class/CSPaddingLabel.swift
@@ -40,18 +40,6 @@ open class CSPaddingLabel: UILabel {
         }
     }
 
-    override open func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
-        let insetRect = bounds.inset(by: edgeInsets)
-        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
-        let invertedInsets = UIEdgeInsets(
-            top: -edgeInsets.top,
-            left: -edgeInsets.left,
-            bottom: -edgeInsets.bottom,
-            right: -edgeInsets.right
-        )
-        return textRect.inset(by: invertedInsets)
-    }
-
     override open func drawText(in rect: CGRect) {
         super.drawText(in: rect.inset(by: edgeInsets))
     }
